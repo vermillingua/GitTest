cf53553010407c9392472c60cd26ef32d1828f9e
Daniël du Preez <dupreezdaniel8@gmail.com>
2021-01-23 22:28:45 -0600

    Initial Commit

---
+++ Shared/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  Shared
+//
+//  Created by Daniël du Preez on 1/23/21.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
